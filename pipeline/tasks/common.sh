6f643ee916d956575d2eaa280deb1eb2839794bd
Tariq Siddiqui <tariqsiddiqui@comcast.net>
2018-02-13 16:56:09 -0500

    Update common.sh

--- pipeline/tasks/common.sh
+++ pipeline/tasks/common.sh
@@ -45,7 +45,8 @@ function login_to_bosh_uaa() {
 	uaa_admin_password=$($CURL --path=/api/v0/deployed/director/credentials/uaa_admin_user_credentials | jq -r .credential.value.password)
 
 	echo "Logging into BOSH UAA..."
-	uaac target https://$director_ip:8443 --skip-ssl-validation
+	# uaac target https://$director_ip:8443 --skip-ssl-validation
+	uaac target https://104.214.117.141:8443 --skip-ssl-validation	
 	uaac token owner get login -s $uaa_login_password<<EOF
 admin
 $uaa_admin_password
